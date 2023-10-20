55ffc6adce1ea35ae9937278d36929c40c9bd396
shawn edgell <shawn_edgell@hotmail.com>
2023-10-20 14:00:00 -0400

    moved build Dir to public folder

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-})
+  build: {
+    outDir: 'public', // Specify the output directory for build files
+  },
+  server: {
+    // Set the root directory to 'public' for the development server
+    // This tells Vite to serve the files from the 'public' folder
+    root: 'public',
+  },
+});
